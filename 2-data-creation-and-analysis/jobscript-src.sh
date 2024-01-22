59b51912e65be08b292f421548d08da69d34f873
Maithili Kalamkar <maithili86@gmail.com>
2024-01-22 15:45:20 +0100

    Create jobscript-src.sh

---
+++ 2-data-creation-and-analysis/jobscript-src.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 
+# Change these variables if you want to change the input or output directories
+inputdir="$HOME/input"
+outputdir="$HOME/result"
+resultsfile="result.txt"
+
+# Make sure that the outputdir exist
+mkdir -p $outputdir
+
+# Count the words of all files in the inputdir and write to outputdirectory
+# The freuqency table will be sorted on most common words first.
+cat $inputdir/* | tr '[:upper:]' '[:lower:]' | awk '{for(i=1;i<=NF;i++) count[$i]++} END {for(j in count) print j, count[j]}' | sort -k2 -n -r > $outputdir/$resultsfile
